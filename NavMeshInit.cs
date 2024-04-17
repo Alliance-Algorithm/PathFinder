e38ddb71738162236ff7a38a8568f641010b4e16
alray <1780284652@qq.com>
2024-04-17 06:55:17 +0000

    Init

---
+++ NavMeshInit.cs
@@ -0,0 +1,10 @@
+namespace AllianceDM.Nav
+{
+    public class NavmeshInit : Component
+    {
+        public NavmeshInit(uint uuid, uint[] revid, string[] args) : base(uuid, revid, args)
+        {
+
+        }
+    }
+}
