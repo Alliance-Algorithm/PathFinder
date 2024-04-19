909048677ec31359c4ea8632ecee9ee9b09d29bb
alray <1780284652@qq.com>
2024-04-19 20:04:22 +0000

    RMUC_SIMPLEVER

--- Global/NavMesh/NavMeshInit.cs
+++ Global/NavMesh/NavMeshInit.cs
@@ -4,7 +4,6 @@ namespace AllianceDM.Nav
     public class NavmeshInit(uint uuid, uint[] revid, string[] args) : Component(uuid, revid, args)
 #pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
     {
-        MapMsg RasterizedMap;
 
         public override void Awake()
         {
--- Global/NavMesh/UnityNavMesh/UnityNav.cs
+++ Global/NavMesh/UnityNavMesh/UnityNav.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using AllianceDM.IO;
 using AllianceDM.StdComponent;
 using Rosidl.Messages.Geometry;
@@ -5,21 +6,22 @@ using Rosidl.Messages.Geometry;
 namespace AllianceDM.Nav
 {
 #pragma warning disable CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
-    public class UnityNav(uint uuid, uint[] revid, string[] args) : Component(uuid, revid, args)
+    public class UnityNav(uint uuid, uint[] revid, string[] args) : GlobalPathFinder(uuid, revid, args)
 #pragma warning restore CS8618 // 在退出构造函数时，不可为 null 的字段必须包含非 null 值。请考虑声明为可以为 null。
     {
         Transform2D SentryPosition;
         Transform2D DestinationPosition;
 
+
         public override void Awake()
         {
-            SentryPosition = DecisionMaker.FindComponent(RecieveID[0]) as Transform2D ?? throw new Exception("UnityNav, id" + ID.ToString());
-            DestinationPosition = DecisionMaker.FindComponent(RecieveID[1]) as Transform2D ?? throw new Exception("UnityNav, id" + ID.ToString());
+            SentryPosition = DecisionMaker.FindComponent<Transform2D>(RecieveID[0]);
+            DestinationPosition = DecisionMaker.FindComponent<Transform2D>(RecieveID[1]);
 
             Task.Run(async () =>
             {
-                using var pub = Ros2Def.node.CreatePublisher<Pose2D>(Args[1]);
-                using var pub2 = Ros2Def.node.CreatePublisher<Pose2D>(Args[2]);
+                using var pub = Ros2Def.node.CreatePublisher<Pose2D>(Args[2]);
+                using var pub2 = Ros2Def.node.CreatePublisher<Pose2D>(Args[3]);
                 using var nativeMsg = pub.CreateBuffer();
                 using var nativeMsg2 = pub2.CreateBuffer();
                 using var timer = Ros2Def.context.CreateTimer(Ros2Def.node.Clock, TimeSpan.FromMilliseconds(value: 1000 / int.Parse(Args[0])));
@@ -35,6 +37,8 @@ namespace AllianceDM.Nav
                     await timer.WaitOneAsync(false);
                 }
             });
+
+            IOManager.RegistryMassage(Args[1], (Pose2D msg) => { DestPos.X = (float)msg.X; DestPos.Y = (float)msg.Y; });
 
         }
         public override void Update()
